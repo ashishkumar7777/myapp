05d4c8f7e47f89953348d0f0ab73d9dba1d5f681
ashish <ashishkumar987820@gmail.com>
2023-06-19 18:19:39 +0530

    newFeature-added

---
+++ src/Component/Child.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Subchild from './Component/Subchild'
+
+const Child = () => {
+    return(
+        <div>
+            <h1>Childd</h1>
+            <Subchild />
+            
+            
+        </div>
+    );
+};
+
+export default Child;
---
+++ src/Component/Subchild.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Subchild = () => {
+    return(
+        <div>
+            <h1>subchild</h1>
+        </div>
+    );
+};
+
+export default Subchild;
